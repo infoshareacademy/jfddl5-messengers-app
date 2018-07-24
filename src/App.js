2dc5c7fb1d3d4a9b861ec5dbd14a08e98a9a0014
Jarek <z.jarek92@gmail.com>
2018-07-24 16:07:04 +0200

    MessagesView added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
 
+import MessagesView from './MessagesView'
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Router>
-          <AppBar
-            title={'Messengers'}
-          />
+          <div>
+            <AppBar
+              title={'Messengers'}
+            />
+
+            <Route path={'/messages'} component={MessagesView} />
+          </div>
         </Router>
       </div>
     );
---
+++ src/MessagesView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MessagesView = () => (
+    <h1>MessagesView</h1>
+)
+
+export default MessagesView
