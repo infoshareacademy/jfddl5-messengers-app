f1543b8440823566b95205b8b8d9536c1eb86fc1
Jarek <z.jarek92@gmail.com>
2018-08-07 16:43:31 +0200

    almost styled

--- src/views/MessagesView/MessagesView.js
+++ src/views/MessagesView/MessagesView.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+
+const displayStyles = {
+    display: 'flex',
+    margin: 20
+}
+
+const containerStyles = {
+
+}
+
+const style = {
+    height: '80vh',
+    width: 'calc(100vw-20)',
+    margin: 20,
+    textAlign: 'center',
+};
 
 
 const MessagesView = () => (
-    <div>
+    <div style={containerStyles}>
+        <div style={displayStyles}>
         <TextField
-            hintText=""
+            hintText="Type your message ..."
             multiLine={true}
             rowsMax={4}
             fullWidth={true}
         />
+        <FlatButton label="Send" />
+        </div>
+        <div><br />
+            <Paper style={style} zDepth={1} />
+        </div>
     </div>
 )
 
