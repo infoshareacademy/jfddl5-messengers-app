90344f8f76f6e58f0b32da286ae026e5623dfd17
Robert Piotrowski <robertpiotrowski93@gmail.com>
2018-08-11 10:27:27 +0200

    getting new message data

--- src/views/MessagesView/MessagesView.js
+++ src/views/MessagesView/MessagesView.js
@@ -24,17 +24,40 @@ const style = {
 
 class MessagesView extends React.Component {
     state = {
-        dateOfMessage: 1533918739807,
-        favourite: false,
-        messageText: '',
-        userAvatar: "https://4vector.com/i/free-vector-ee-train-clip-art_116884_Ee_Train_clip_art_small.png",
-        userId: 767678686778
+        newMessage: {
+            dateOfMessage: 1533918739807,
+            favourite: false,
+            messageText: '',
+            userAvatar: "https://4vector.com/i/free-vector-ee-train-clip-art_116884_Ee_Train_clip_art_small.png",
+            userId: 767678686778
+        },
+        allMessages: null
+    }
+
+    getMesseges = () => {
+        const request = {
+            method: 'GET'
+        }
+        fetch('https://jfddl5-messengers.firebaseio.com/messeges/-LJUAF34bUu4jb-xz4wl.json', request)
+            .then(response => response.json())
+            .then(responseData => {
+                const newArr = Object.entries(responseData)
+                return newArr.map(el => (
+                    {
+                        id: el[0],
+                        ...el[1]
+                    }
+                ))
+            })
+            .then(data => this.setState({
+                allMessages: data
+            }))
     }
 
     sendMessageText = () => {
         const request = {
             method: 'POST',
-            body: JSON.stringify(this.state)
+            body: JSON.stringify(this.state.newMessage)
         }
         fetch('https://jfddl5-messengers.firebaseio.com/messeges/-LJUAF34bUu4jb-xz4wl.json', request)
     }
@@ -42,34 +65,40 @@ class MessagesView extends React.Component {
     handleChange = (event) => {
         const text = event.target.value
         this.setState({
-            dateOfMessage: Date.now(),
-            messageText: text
+            newMessage: {
+                dateOfMessage: Date.now(),
+                favourite: false,
+                messageText: text,
+                userAvatar: "https://4vector.com/i/free-vector-ee-train-clip-art_116884_Ee_Train_clip_art_small.png",
+                userId: 767678686778
+            }
         })
     }
-    
-    render(){
+
+    render() {
         return (
             <div style={containerStyles}>
-            <div style={displayStyles}>
-            <TextField
-                hintText="Type your message ..."
-                multiLine={true}
-                rowsMax={4}
-                fullWidth={true}
-                value={this.state.name}
-                onChange={this.handleChange}
-                />
-                {console.log(this.state.messageText)}
-            <FlatButton label="Send" name='name' 
-            onClick={this.sendMessageText} 
-            />
+                <div style={displayStyles}>
+                {console.log(this.state.allMessages)}
+                    <TextField
+                        hintText="Type your message ..."
+                        multiLine={true}
+                        rowsMax={4}
+                        fullWidth={true}
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                    {console.log(this.state.messageText)}
+                    <FlatButton label="Send" name='name'
+                        onClick={this.getMesseges}
+                    />
+                </div>
+                <div><br />
+                    <Paper style={style} zDepth={1} >
+                        <List />
+                    </Paper>
+                </div>
             </div>
-            <div><br />
-                <Paper style={style} zDepth={1} >
-                <List />
-                </Paper>
-            </div>
-        </div>
         )
     }
 }
