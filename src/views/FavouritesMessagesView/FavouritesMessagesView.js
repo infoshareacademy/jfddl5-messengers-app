bb15ad8c030bb6d958a9cf13db628b612ba7c975
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-08-13 13:15:34 +0200

    done favourite

--- src/views/FavouritesMessagesView/FavouritesMessagesView.js
+++ src/views/FavouritesMessagesView/FavouritesMessagesView.js
@@ -1,7 +1,90 @@
 import React from 'react'
 
-const FavouritesMessagesView = () => (
-    <h1>FavouritesMessagesView</h1>
+
+
+class FavouritesMessagesView  extends React.Component{
+
+
+
+state = {
+
+    data: null,
+
+    list:[]
+
+   
+
+}
+
+componentDidMount () {
+
+fetch('https://jfddl5-messengers.firebaseio.com/messeges/-LJUAF34bUu4jb-xz4wl.json')
+
+.then(response => response.json())
+
+.then(dataResponse => {
+
+const newArrayData = Object.entries(dataResponse || {})
+
+return (newArrayData.map(el => ({
+
+    id:el[0],
+
+    ...el[1]}
+
+)))})
+
+.then(newArrayData => newArrayData.filter(function(favo,index,array){
+
+    return favo.favourite === false
+
+}))
+
+.then(data => data.map(el=>{
+
+    return <p>{el.messageText}</p>
+
+}))
+
+.then(data=>
+
+this.setState({list:data})
+
+
 )
+
+}
+
+    render(){
+
+    return(
+
+        <div>
+
+            <div>
+
+    <h1>FavouritesMessagesView</h1>
+
+          <div>
+
+    <h2>{this.state.list}</h2>
+
+         </div>
+
+            </div>
+
+</div>
+
+)}
+
+}
+
+
+
+    
+
+
+
+
 
 export default FavouritesMessagesView
