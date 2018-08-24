f862b8fb30ab8bc129a23fcb2281d6955cfe53f3
Robert Piotrowski <piotrowskirobert7@gmail.com>
2018-08-24 15:12:42 +0200

    sending new messages

--- src/views/ChannelsView/ChannelsView.js
+++ src/views/ChannelsView/ChannelsView.js
@@ -3,7 +3,7 @@ import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
 import ListOfMessages from '../../components/ListOfMessages/ListOfMessages'
 import { connect } from 'react-redux'
-import { saveMessagesAction, handleFavourite, startChannelSync, stopChannelSync } from '../../state/messageView'
+import { saveMessagesAction, handleFavourite, startChannelSync, stopChannelSync, handleNewMessageText, sendNewMessageText } from '../../state/messageView'
 
 const displayStyles = {
     display: 'flex',
@@ -45,8 +45,9 @@ class ChannelsView extends React.Component {
                         multiLine={true}
                         rowsMax={4}
                         fullWidth={true}
+                        onChange={(event)=>this.props.handleNewMessageText(event.target.value)}
                     />
-                    <FlatButton label="Send" name='name'
+                    <FlatButton label="Send" name='name' onClick={()=>this.props.sendNewMessageText(channelId)}
                     />
                 </div>
                 <div >
@@ -69,7 +70,9 @@ const mapDispatchToProps = dispatch => ({
     saveMessagesAction: (data) => dispatch(saveMessagesAction(data)),
     handleFavourite: (id) => dispatch(handleFavourite(id)),
     startChannelSync: (channelId) => dispatch(startChannelSync(channelId)),
-    stopChannelSync: (channelId) => dispatch(stopChannelSync(channelId))
+    stopChannelSync: (channelId) => dispatch(stopChannelSync(channelId)),
+    handleNewMessageText: (value) => dispatch(handleNewMessageText(value)),
+    sendNewMessageText: (channelId)=> dispatch(sendNewMessageText(channelId))
 })
 
 
