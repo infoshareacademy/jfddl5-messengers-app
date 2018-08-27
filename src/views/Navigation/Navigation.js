8e4b04468488c88b4fc7eacabfb534d655e5fe31
Robert Piotrowski <piotrowskirobert7@gmail.com>
2018-08-27 08:35:46 +0200

    new logout button

--- src/views/Navigation/Navigation.js
+++ src/views/Navigation/Navigation.js
@@ -3,12 +3,21 @@ import AppBar from 'material-ui/AppBar'
 import SideBar from './SideBar'
 import { connect } from 'react-redux'
 import { toggleSidebarAction } from '../../state/navigation'
+import FlatButton from 'material-ui/FlatButton';
+import {onLogOutAction} from '../../state/auth'
+
 
 const Navigation = (props) => (
     <div>
         <AppBar
             title={'Messengers'}
             onLeftIconButtonClick={props._toggleSidebarAction}
+            iconElementRight={
+                <FlatButton
+                    label="Logout"
+                    onClick={props._onLogOutAction}
+                />
+            }
         />
         <SideBar
             toggleHandler={props._toggleSidebarAction}
@@ -22,7 +31,9 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-    _toggleSidebarAction: () => dispatch(toggleSidebarAction())
+    _toggleSidebarAction: () => dispatch(toggleSidebarAction()),
+    _onLogOutAction: () => dispatch(onLogOutAction()),
+
 })
 
 export default connect(
